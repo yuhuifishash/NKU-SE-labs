2c92e722135fb5ec9c9efd3bfda8439092bab244
HW-Z-2111025 <2111025@mail.nankai.edu.cn>
2024-06-16 00:10:49 +0800

    更改通知列表样式

--- frontend/src/components/user/Notice.vue
+++ frontend/src/components/user/Notice.vue
@@ -1,20 +1,24 @@
 <template>
-    <div class="main">
-    <h1>通知列表</h1>
+  <div class="main">
+    <h1 class="main-title">通知列表</h1>
 
-     <div v-for="item in Notices" :key='item' class="notice">
-        <div class="photo">
-            <el-avatar :size='50' :style="`background:${extractColorByName('{{item.Sender}}')}`"> {{item.Sender}} </el-avatar>
-            <span class="notice_name">{{item.Sender}}</span>
+    <div v-for="item in Notices" :key='item' class="notice">
+      <div class="photo">
+        <el-avatar :size='50' :style="`background:${extractColorByName(item.Sender)}`">{{ item.Sender }}</el-avatar>
+        <div class="photo-details">
+          <span class="notice_name">{{ item.Sender }}</span>
+          <p class="notice_time">{{ item.Time }}</p>
         </div>
+      </div>
 
-        <p class="notice_time">{{ item.Time }}</p>
-        <h2 v-if="item.Type=='Warn'" class="warn">
-            数据异常警告
-        </h2>
-        <p class="notice_content">{{ item.Content }}</p>
+      <div class="notice_details">
+        <div class="notice_content_wrapper">
+          <h2 v-if="item.Type=='Warn'" class="warn">数据异常警告</h2>
+          <p class="notice_content">{{ item.Content }}</p>
+        </div>
       </div>
     </div>
+  </div>
 </template>
 
 <script>
@@ -48,6 +52,7 @@ export default {
         }
       }).then(res => {
         this.Notices = res.data
+        console.log(res.data)
       }).catch(err => {
         console.log(err)
       })
@@ -67,47 +72,89 @@ export default {
 
 <style scoped>
 .main {
-  max-width: 80%;
+  max-width: 60%;
   margin: 0 auto;
-  margin-top: 5px;
-  padding: 5px;
+  margin-top: 20px;
+  padding: 20px;
   box-shadow: 0 4px 8px rgba(0,0,0,0.1);
+  background-color: #f9f9f9;
+  border-radius: 10px;
 }
-.main-title, .admin-title {
-  font-size: 24px;
+
+.main-title {
+  font-size: 28px;
   text-align: center;
-  margin-bottom: 20px;
+  margin-bottom: 30px;
+  color: #333;
+  font-weight: bold;
 }
+
 .notice {
-    border-radius: 50px 50px 50px 50px;
-    padding: 3px;
-    margin: 10px;
-    background-color: #fff;
+  border-radius: 10px;
+  padding: 15px;
+  margin: 20px 0;
+  background-color: #fff;
+  box-shadow: 0 2px 4px rgba(0,0,0,0.1);
+  transition: transform 0.2s;
 }
+
+.notice:hover {
+  transform: translateY(-5px);
+}
+
 .photo {
-    margin-left: 150px;
-    width: 300px;
-    height: 20px;
+  display: flex;
+  align-items: center;
+  margin-bottom: 10px;
 }
-.notice_time {
-    font-size:20px
+
+.el-avatar {
+  margin-right: 10px;
 }
+
+.photo-details {
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+  justify-content: center;
+}
+
 .notice_name {
-    font-size:20px;
-    color:#292b30;
-    text-shadow: 0 8px 10px #292b30;
-    font-weight: bolder;
-    text-align: center;
+  font-size: 20px;
+  color: #333;
+  font-weight: bold;
+  margin-bottom: 2px;
+  text-align: left;
 }
+
+.notice_time {
+  font-size: 14px;
+  color: #999;
+  margin-top: 2px;
+  text-align: left;
+}
+
+.notice_details {
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+}
+
+.notice_content_wrapper {
+  width: 100%;
+}
+
 .notice_content {
-    margin-left: 40px;
-    margin-right: 40px;
+  font-size: 16px;
+  line-height: 1.5;
+  color: #555;
 }
+
 .warn {
-    font-size:25px;
-    color:#ff0d01;
-    text-shadow: 0 8px 10px #ff6a00;
-    font-weight: bolder;
-    text-align: center;
+  font-size: 20px;
+  color: #ff0000;
+  font-weight: bold;
+  margin-bottom: 10px;
+  text-align: center;
 }
 </style>
